36c8c429a77c9c556afd7cd5d1d57b118f14c199
SabineZilde <sabine.zilde@protonmail.ch>
2021-05-29 14:07:58 +0300

    Adding some id's in cart page

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Cart page functionality
+
+let productQty1 = 1;
+let productQty2 = 1;
+let productQty3 = 1;
+
+$('#add').on('click', () => {
+    $('#product1').html(productQty1 += 1);
+});
+
+$('#subtract').on('click', () => {
+    $('#product1').html(productQty1 -= 1);
+});
