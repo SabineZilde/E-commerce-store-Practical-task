972ca39df686e977d8d2121a690904a41893b1c7
SabineZilde <sabine.zilde@protonmail.ch>
2021-05-30 15:41:18 +0300

    js

--- js/main.js
+++ js/main.js
@@ -19,12 +19,15 @@ let newTotalProd2 = 3.50;
 let totalProduct3 = 6.00;
 
 let subtotal1 = 0;
+let subtotal2 = 0;
+let subtotal = 0;
 
 
 $('#add1').on('click', () => {
     $('#product1').html(productQty1 += 1);
     $('#total1').html(newTotalProd1 += totalProduct1);
     $('#subtotal').html(subtotal1 = newTotalProd1);
+
 });
 
 $('#subtract1').on('click', () => {   
@@ -37,7 +40,10 @@ $('#subtract1').on('click', () => {
 $('#add2').on('click', () => {
     $('#product2').html(productQty2 += 1);
     $('#total2').html(newTotalProd2 += totalProduct2);
+    $('#subtota2').html(subtotal2 = newTotalProd2);
 });
 
-let subtotal = subtotal1;
+setInterval( () => {
+    subtotal = subtotal1 + subtotal2;
+}, 10);
 
