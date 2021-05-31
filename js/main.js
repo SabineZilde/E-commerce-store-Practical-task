8fc6689b75011c75016f4db1b2b167fbc4fe5740
SabineZilde <sabine.zilde@protonmail.ch>
2021-05-31 16:43:24 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -78,28 +78,30 @@ setInterval(() => {
         $('#subtotal').html(subtotal = 0);
         $('#emptycart').removeAttr('hidden');
         $('#finaltotal').html(subtotal);
+        
     } else if (clicked1 > 0 && clicked2 > 0) {
         $('#subtotal').html(subtotal = subtotal3);
-    $('#finaltotal').html(subtotal + shipping);
+        $('#finaltotal').html(subtotal + shipping);
+
     } else if (clicked2 > 0 && clicked3 > 0) {
         $('#subtotal').html(subtotal = subtotal1);
-    $('#finaltotal').html(subtotal + shipping);
+        $('#finaltotal').html(subtotal + shipping);
 
     } else if (clicked1 > 0 && clicked3 > 0) {
         $('#subtotal').html(subtotal = subtotal2);
-    $('#finaltotal').html(subtotal + shipping);
+        $('#finaltotal').html(subtotal + shipping);
 
     } else if (clicked1 > 0) {
         $('#subtotal').html(subtotal = subtotal2 + subtotal3);
-    $('#finaltotal').html(subtotal + shipping);
+        $('#finaltotal').html(subtotal + shipping);
 
     } else if (clicked2 > 0) {
         $('#subtotal').html(subtotal = subtotal1 + subtotal3);
-    $('#finaltotal').html(subtotal + shipping);
+        $('#finaltotal').html(subtotal + shipping);
 
     } else if (clicked3 > 0) {
         $('#subtotal').html(subtotal = subtotal1 + subtotal2);
-    $('#finaltotal').html(subtotal + shipping);
+        $('#finaltotal').html(subtotal + shipping);
 
     }
 
